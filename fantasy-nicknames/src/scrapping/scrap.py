b88ec3e083d9a671cef8aaf429fbad2ae1d4d7a7
Justin <justinjp126@gmail.com>
2022-10-10 16:17:02 -0400

    Add pickens, but otherPlayers() still not working

--- fantasy-nicknames/src/scrapping/scrap.py
+++ fantasy-nicknames/src/scrapping/scrap.py
@@ -6,123 +6,122 @@ import json
 import re
 
 # nickname variable links
-j_taylor_source = requests.get(
-    "https://www.sportskeeda.com/nfl/40-jonathan-taylor-fantasy-football-names").text
+# j_taylor_source = requests.get(
+#    "https://www.sportskeeda.com/nfl/40-jonathan-taylor-fantasy-football-names")
 n_harris_source = requests.get(
-    'https://www.sportskeeda.com/nfl/40-najee-harris-fantasy-football-team-names').text
+    'https://www.sportskeeda.com/nfl/40-najee-harris-fantasy-football-team-names')
 a_ekeler_source = requests.get(
     'https://friendsgroupname.com/austin-ekeler-fantasy-names/').text
 c_kupp_source = requests.get(
-    'https://www.ranker.com/list/best-cooper-kupp-fantasy-name/pedro-cerrano').text
+    'https://www.ranker.com/list/best-cooper-kupp-fantasy-name/pedro-cerrano')
 d_henry_source = requests.get(
-    'https://www.ranker.com/list/best-derrick-henry-fantasy-name/pedro-cerrano').text
+    'https://www.ranker.com/list/best-derrick-henry-fantasy-name/pedro-cerrano')
 c_mccaffrey_source = requests.get(
-    'https://www.sportskeeda.com/nfl/40-christian-mccaffrey-fantasy-football-team').text
+    'https://www.sportskeeda.com/nfl/40-christian-mccaffrey-fantasy-football-team')
 a_kamara_source = requests.get(
-    'https://www.sportskeeda.com/nfl/40-alvin-kamara-fantasy-football-team-name-2022').text
+    'https://www.sportskeeda.com/nfl/40-alvin-kamara-fantasy-football-team-name-2022')
 d_cook_source = requests.get(
     'https://www.sportsfeelgoodstories.com/dalvin-cook-fantasy-team-names/').text
 j_jefferson_source = requests.get(
-    'https://www.ranker.com/list/best-justin-jefferson-team-name/pedro-cerrano').text
+    'https://www.ranker.com/list/best-justin-jefferson-team-name/pedro-cerrano')
 j_chase_source = requests.get(
-    'https://www.ranker.com/list/best-jamarr-chase-fantasy-name/pedro-cerrano').text
+    'https://www.ranker.com/list/best-jamarr-chase-fantasy-name/pedro-cerrano')
 c_lamb_source = requests.get(
-    'https://www.cheatsheetwarroom.com/blog/fantasy-football/names/wide-receiver-names/ceedee-lamb').text
+    'https://www.cheatsheetwarroom.com/blog/fantasy-football/names/wide-receiver-names/ceedee-lamb')
 s_diggs_source = requests.get(
-    'https://www.ranker.com/list/best-stefon-diggs-fantasy-name/pedro-cerrano').text
+    'https://www.ranker.com/list/best-stefon-diggs-fantasy-name/pedro-cerrano')
 d_adams_source = requests.get(
-    'https://www.sportskeeda.com/nfl/30-best-davante-adams-fantasy-football-names-try-2022').text
+    'https://www.sportskeeda.com/nfl/30-best-davante-adams-fantasy-football-names-try-2022')
 d_swift_source = requests.get(
-    'https://www.ranker.com/list/best-dandre-swift-fantasy-name/pedro-cerrano').text
+    'https://www.ranker.com/list/best-dandre-swift-fantasy-name/pedro-cerrano')
 t_kelce_source = requests.get(
-    'https://www.cheatsheetwarroom.com/blog/fantasy-football/names/tight-end-names/travis-kelce').text
+    'https://www.cheatsheetwarroom.com/blog/fantasy-football/names/tight-end-names/travis-kelce')
 m_andrews_source = requests.get(
-    'https://www.sportskeeda.com/nfl/news-mark-andrews-fantasy-football-team-names').text
+    'https://www.sportskeeda.com/nfl/news-mark-andrews-fantasy-football-team-names')
 j_mixon_source = requests.get(
-    'https://www.ranker.com/list/best-joe-mixon-fantasy-name/pedro-cerrano').text
+    'https://www.ranker.com/list/best-joe-mixon-fantasy-name/pedro-cerrano')
 m_evans_source = requests.get(
-    'https://www.fanduel.com/theduel/posts/mike-evans-fantasy-football-team-names-01ga9p6ym5j5').text
+    'https://www.fanduel.com/theduel/posts/mike-evans-fantasy-football-team-names-01ga9p6ym5j5')
 t_hill_source = requests.get(
-    'https://www.sportskeeda.com/nfl/40-tyreek-hill-fantasy-football-team-names').text
+    'https://www.sportskeeda.com/nfl/40-tyreek-hill-fantasy-football-team-names')
 d_samuel_source = requests.get(
-    'https://www.sportskeeda.com/nfl/deebo-samuel-fantasy-football-team-names').text
+    'https://www.sportskeeda.com/nfl/deebo-samuel-fantasy-football-team-names')
 k_allen_source = requests.get(
-    'https://www.fanduel.com/theduel/posts/keenan-allen-fantasy-football-team-names-2021-season-01fepgksvcmf').text
+    'https://www.fanduel.com/theduel/posts/keenan-allen-fantasy-football-team-names-2021-season-01fepgksvcmf')
 l_fournette_source = requests.get(
-    'https://flurrysports.org/leonard-fournette-fantasy-football-team-names/').text
+    'https://flurrysports.org/leonard-fournette-fantasy-football-team-names/')
 j_williams_source = requests.get(
-    'https://flurrysports.org/javonte-williams-fantasy-football-team-names/').text
+    'https://flurrysports.org/javonte-williams-fantasy-football-team-names/')
 t_mclaurin_source = requests.get(
-    'https://www.ranker.com/list/best-terry-mclaurin-fantasy-name/pedro-cerrano').text
+    'https://www.ranker.com/list/best-terry-mclaurin-fantasy-name/pedro-cerrano')
 d_johnson_source = requests.get(
-    'https://www.drodd.com/fantasyteamname/diontae-johnson-fantasy-names.html').text
+    'https://www.drodd.com/fantasyteamname/diontae-johnson-fantasy-names.html')
 a_jones_source = requests.get(
-    'https://www.fanduel.com/theduel/posts/aaron-jones-fantasy-football-team-names-2021-season-01fe42v38eft').text
+    'https://www.fanduel.com/theduel/posts/aaron-jones-fantasy-football-team-names-2021-season-01fe42v38eft')
 e_elliott_source = requests.get(
-    'https://flurrysports.org/ezekiel-elliott-fantasy-football-team-names/').text
+    'https://flurrysports.org/ezekiel-elliott-fantasy-football-team-names/')
 k_pitts_source = requests.get(
-    'https://flurrysports.org/kyle-pitts-fantasy-football-team-names/').text
+    'https://flurrysports.org/kyle-pitts-fantasy-football-team-names/')
 a_brown_source = requests.get(
-    'https://flurrysports.org/a-j-brown-fantasy-football-team-names/').text
+    'https://flurrysports.org/a-j-brown-fantasy-football-team-names/')
 c_akers_source = requests.get(
-    'https://www.fanduel.com/theduel/posts/cam-akers-fantasy-football-team-names-01ga9qd3frr0').text
+    'https://www.fanduel.com/theduel/posts/cam-akers-fantasy-football-team-names-01ga9qd3frr0')
 j_conner_source = requests.get(
-    'https://www.fanduel.com/theduel/posts/james-conner-fantasy-football-team-names-01ga9pxxc3d5').text
+    'https://www.fanduel.com/theduel/posts/james-conner-fantasy-football-team-names-01ga9pxxc3d5')
 s_barkley_source = requests.get(
-    'https://www.ranker.com/list/best-saquon-barkley-fantasy-name/pedro-cerrano').text
+    'https://www.ranker.com/list/best-saquon-barkley-fantasy-name/pedro-cerrano')
 j_allen_source = requests.get(
-    'https://www.cheatsheetwarroom.com/blog/fantasy-football/names/quarterback-names/josh-allen').text
+    'https://www.cheatsheetwarroom.com/blog/fantasy-football/names/quarterback-names/josh-allen')
 n_chubb_source = requests.get(
-    'https://www.sportskeeda.com/nfl/news-40-nick-chubb-fantasy-football-team-names-2022').text
+    'https://www.sportskeeda.com/nfl/news-40-nick-chubb-fantasy-football-team-names-2022')
 d_metcalf_source = requests.get(
-    'https://www.sportskeeda.com/nfl/40-dk-metcalf-fantasy-football-team-names').text
+    'https://www.sportskeeda.com/nfl/40-dk-metcalf-fantasy-football-team-names')
 d_moore_source = requests.get(
-    'https://www.drodd.com/fantasyteamname/dj-moore-fantasy-names.html').text
+    'https://www.drodd.com/fantasyteamname/dj-moore-fantasy-names.html')
 j_jacobs_source = requests.get(
-    'https://www.cheatsheetwarroom.com/blog/fantasy-football/names/running-back-names/josh-jacobs').text
+    'https://www.cheatsheetwarroom.com/blog/fantasy-football/names/running-back-names/josh-jacobs')
 c_helaire_source = requests.get(
-    'https://www.fanduel.com/theduel/posts/clyde-edwards-helaire-fantasy-football-team-names-2021-season-01fepjgt4qvd').text
+    'https://www.fanduel.com/theduel/posts/clyde-edwards-helaire-fantasy-football-team-names-2021-season-01fepjgt4qvd')
 d_waller_source = requests.get(
-    'https://www.fanduel.com/theduel/posts/darren-waller-fantasy-football-team-names-2021-season-01fep5s798cc').text
+    'https://www.fanduel.com/theduel/posts/darren-waller-fantasy-football-team-names-2021-season-01fep5s798cc')
 g_kittle_source = requests.get(
-    'https://www.ranker.com/list/best-george-kittle-fantasy-name/pedro-cerrano').text
+    'https://www.ranker.com/list/best-george-kittle-fantasy-name/pedro-cerrano')
 c_patterson_source = requests.get(
-    'https://www.fanduel.com/theduel/posts/cordarrelle-patterson-fantasy-football-team-names-2021-waiver-wire-01fg521qbqev').text
+    'https://www.fanduel.com/theduel/posts/cordarrelle-patterson-fantasy-football-team-names-2021-waiver-wire-01fg521qbqev')
 t_higgins_source = requests.get(
-    'https://www.sportskeeda.com/nfl/40-tee-higgins-fantasy-football-team-names').text
+    'https://www.sportskeeda.com/nfl/40-tee-higgins-fantasy-football-team-names')
 e_mitchell_source = requests.get(
-    'https://flurrysports.org/elijah-mitchell-fantasy-football-team-names/').text
+    'https://flurrysports.org/elijah-mitchell-fantasy-football-team-names/')
 j_herbert_source = requests.get(
-    'https://www.cheatsheetwarroom.com/blog/fantasy-football/names/quarterback-names/justin-hebert').text
+    'https://www.cheatsheetwarroom.com/blog/fantasy-football/names/quarterback-names/justin-hebert')
 p_mahomes_source = requests.get(
-    'https://www.ranker.com/list/best-patrick-mahomes-fantasy-name/pedro-cerrano').text
+    'https://www.ranker.com/list/best-patrick-mahomes-fantasy-name/pedro-cerrano')
 j_waddle_source = requests.get(
-    'https://flurrysports.org/jaylen-waddle-fantasy-football-team-names/').text
-
+    'https://flurrysports.org/jaylen-waddle-fantasy-football-team-names/')
 l_jackson_source = requests.get(
-    'https://www.sportskeeda.com/nfl/40-lamar-jackson-fantasy-football-team-names').text
+    'https://www.sportskeeda.com/nfl/40-lamar-jackson-fantasy-football-team-names')
 j_hurts_source = requests.get(
-    'https://www.ranker.com/list/best-jalen-hurts-fantasy-name/pedro-cerrano').text
+    'https://www.ranker.com/list/best-jalen-hurts-fantasy-name/pedro-cerrano')
 k_murray_source = requests.get(
-    'https://www.drodd.com/fantasyteamname/kyler-murray-fantasy-team-names.html').text
+    'https://www.drodd.com/fantasyteamname/kyler-murray-fantasy-team-names.html')
 j_goff_source = requests.get(
-    'https://www.drodd.com/fantasyteamname/jared-goff-fantasy-names.html').text
+    'https://www.drodd.com/fantasyteamname/jared-goff-fantasy-names.html')
 j_burrow_source = requests.get(
-    'https://www.ranker.com/list/best-joe-burrow-fantasty-name/pedro-cerrano').text
+    'https://www.ranker.com/list/best-joe-burrow-fantasty-name/pedro-cerrano')
 t_tagovailoa_source = requests.get(
-    'https://www.ranker.com/list/best-tua-tagovailoa-fantasy-name/pedro-cerrano').text
+    'https://www.ranker.com/list/best-tua-tagovailoa-fantasy-name/pedro-cerrano')
 t_brady_source = requests.get(
-    'https://www.sportskeeda.com/nfl/tom-brady-fantasy-football-team-names-cool-funny-ideas-2022').text
+    'https://www.sportskeeda.com/nfl/tom-brady-fantasy-football-team-names-cool-funny-ideas-2022')
 am_brown_source = requests.get(
-    'https://www.cheatsheetwarroom.com/blog/fantasy-football/names/wide-receiver-names/amon-ra-st-brown').text
+    'https://www.cheatsheetwarroom.com/blog/fantasy-football/names/wide-receiver-names/amon-ra-st-brown')
 k_hunt_source = requests.get(
-    'https://flurrysports.org/kareem-hunt-fantasy-football-team-names/').text
+    'https://flurrysports.org/kareem-hunt-fantasy-football-team-names/')
 j_robinson_source = requests.get(
-    'https://www.fanduel.com/theduel/posts/james-robinson-fantasy-football-team-names-01ek2t9p4b49').text
+    'https://www.fanduel.com/theduel/posts/james-robinson-fantasy-football-team-names-01ek2t9p4b49')
 t_hockenson_source = requests.get(
-    'https://flurrysports.org/t-j-hockenson-fantasy-football-team-names/').text
+    'https://flurrysports.org/t-j-hockenson-fantasy-football-team-names/')
 g_pickens_source = requests.get(
-    'https://flurrysports.org/george-pickens-fantasy-football-team-names/').text
+    'https://flurrysports.org/george-pickens-fantasy-football-team-names/')
 namesDic = {}  # dictionary with name as key, array of nicknames as value
 
 '''
@@ -133,6 +132,7 @@ ranker
 
 
 def addRankerPlayer(source, playerName):
+    source = source.text
     soup = BeautifulSoup(source, 'lxml')
     namesDic.update({playerName: []})
     nicknameTags = soup.find_all("h2", class_="listItem_name__Qq_Y8")
@@ -151,6 +151,7 @@ Sportkeeda
 
 
 def addSportkeedaPlayer(source, playerName):
+    source = source.text
     soup = BeautifulSoup(source, 'lxml')
     namesDic.update({playerName: []})
     nicknameTags = soup.find_all('li', class_=None, id=None)
@@ -169,6 +170,7 @@ cheat sheet
 
 
 def addCheatSheetPlayer(source, playerName):
+    source = source.text
     soup = BeautifulSoup(source, 'lxml')
     namesDic.update({playerName: []})
     nicknameTags = soup.find_all("span", class_="auton")
@@ -186,6 +188,7 @@ FanDual website
 
 
 def addFanDualPlayer(source, playerName):
+    source = source.text
     soup = BeautifulSoup(source, 'lxml')
     namesDic.update({playerName: []})
     nicknameTags = soup.find_all("h2", {"data-mm-id": True})
@@ -207,6 +210,7 @@ Flurry Sports website
 
 
 def addFlurrySportsPlayer(source, playerName):
+    source = source.text
     soup = BeautifulSoup(source, 'lxml')
     namesDic.update({playerName: []})
     playerHeading = soup.find("ul", class_=None)
@@ -255,7 +259,7 @@ def addOtherPlayers():
 
 
 def addPlayersToDictionary():
-    addSportkeedaPlayer(j_taylor_source, "Johnathan Taylor")
+    #addSportkeedaPlayer(j_taylor_source, "Johnathan Taylor")
     addSportkeedaPlayer(n_harris_source, "Najee Harris")
     addRankerPlayer(c_kupp_source, "Cooper Kupp")
     addRankerPlayer(d_henry_source, "Derrick Henry")
@@ -309,7 +313,7 @@ def addPlayersToDictionary():
     addFanDualPlayer(j_robinson_source, "James Robinson")
     addFlurrySportsPlayer(t_hockenson_source, "TJ Hockenson")
     addFlurrySportsPlayer(g_pickens_source, "George Pickens")
-    addOtherPlayers()
+    # addOtherPlayers()
 
 
 def createJSONFile():
@@ -320,7 +324,7 @@ def createJSONFile():
 
 def main():
     addPlayersToDictionary()
-    print(namesDic.keys())
+    print(namesDic)
     # createJSONFile()
 
 
