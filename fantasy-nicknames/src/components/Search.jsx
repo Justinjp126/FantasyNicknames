b6b0ca04bfe212fb2d9a3972322f0213b4d38356
Justin <justinjp126@gmail.com>
2022-10-11 14:19:51 -0400

    Get datalist working using .map :)

--- fantasy-nicknames/src/components/Search.jsx
+++ fantasy-nicknames/src/components/Search.jsx
@@ -1,9 +1,28 @@
 import React from "react";
-
+import fantasyNicknames from "../fantasy-nicknames.json"
+import Options from "./Options";
 export default function Search() {
+  var namesArray = []
+  Object.keys(fantasyNicknames).forEach(function(key,index) {
+    namesArray.push(key)
+  })
+  const names = namesArray.map((items) => {
+    return (
+      <Options 
+      items={items}
+      />
+    )
+  })
+
   return (
     <>
-      <input placeholder="Johnathon Taylor" type="search" name="Enter Player Name" id="searchBar" className="search" />
+    <form action="">
+      <input placeholder="Enter Player Name" list="search_bar" name="searchBar" id="searchBar" className="search" />
+      <datalist id="search_bar">
+        {names}
+      </datalist>
+    </form>
+          
     </>
     
   );
