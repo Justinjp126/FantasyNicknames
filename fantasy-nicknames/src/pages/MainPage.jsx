e11c002bdba06ef917efd38d4527be75dca1feb8
Justin <justinjp126@gmail.com>
2022-10-29 20:27:25 -0400

    Fix issue with errors when Firebase was loading in data and added transition to fill loading time

--- fantasy-nicknames/src/pages/MainPage.jsx
+++ fantasy-nicknames/src/pages/MainPage.jsx
@@ -1,13 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Header from "../components/Header"
 import Search from "../components/Search";
-import Player from "../components/Player";
-import Footer from "../components/Footer"
-import fantasyNicknames from "../fantasy-nicknames.json"
-import { Link } from "react-router-dom";
-import { db } from "../firebase"
-import { getDatabase, ref, onValue } from "firebase/database";
-
 import PopularPlayers from "../components/PopularPlayers";
 
 export default function MainPage() {
